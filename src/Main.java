d4b3bcb44753824b34b2bdf84bab9fa9e60b299b
Shoaib Umer <shabiumer@hotmail.com>
2019-01-06 18:27:09 +0000

    addition function added

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
+
+        int x=10;
+        int y=20;
+
         System.out.println("Hello World!");
+        System.out.println(x+y);
     }
 }
