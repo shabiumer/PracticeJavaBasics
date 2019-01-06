215a31644dc2c307aee3cd16a62920b3bf6f59f4
Shoaib Umer <shabiumer@hotmail.com>
2019-01-06 18:46:31 +0000

    multiplication class added

---
+++ src/Multiplication.java
@@ -0,0 +1,11 @@
+public class Multiplication {
+
+    public static void  main(String[] args){
+
+        int a = 5;
+        int b = 2;
+        int c = a * b;
+
+        System.out.println("The multiplication of a x b is: "+c);
+    }
+}
